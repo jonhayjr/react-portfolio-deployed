0c71e3e147f4a617eee6a2f454f4ebbfa3fd4e65
jonhayjr <jonhayjr@gmail.com>
2021-08-10 07:48:39 -0700

    Added changes to skills component.  HandleClick inverts skill colors when it is clicked

--- client/src/Navbar/Navbar.js
+++ client/src/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
     return (
@@ -9,22 +9,22 @@ const Navbar = () => {
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse navbar-toggleable-xs justify-content-md-end text-center" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item mx-3">
-                    <a className="nav-link" aria-current="page" href="/#home">Home</a>
-                    </li>
-                    <li className="nav-item mx-3">
-                    <a className="nav-link" href="/#about">About</a>
-                    </li>
-                    <li className="nav-item mx-3">
-                    <a className="nav-link" href="/#projects">Projects</a>
-                    </li>
-                    <li className="nav-item mx-3">
-                    <a className="nav-link" href="/#skills">Skills</a>
-                    </li>
-                    <li className="nav-item mx-3">
-                    <a className="nav-link" href="/#contact">Contact</a>
-                    </li>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item mx-3">
+                            <a className="nav-link" aria-current="page" href="/#home">Home</a>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link" href="/#about">About</a>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link" href="/#projects">Projects</a>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link" href="/#skills">Skills</a>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link" href="/#contact">Contact</a>
+                        </li>
                     </ul>
                 </div>
             </div>
--- client/src/Skills/Skills.js
+++ client/src/Skills/Skills.js
@@ -1,4 +1,22 @@
 const Skills = () => {
+    const handleClick = (e) => {
+        //Get current skill and make lowercase
+        const skill = e.target.innerText.toLowerCase();
+        //Get current class
+        const currentClass = e.target.className;
+        //Initialize new class variable
+        let newClass;
+        
+        //If class contains inverted, replace with blank.  If it doesn't contain inverted, add it.
+        if (currentClass.indexOf('inverted') >= 0) {
+            newClass = currentClass.replace('-inverted', '');
+        } else {
+            newClass = currentClass.replace(`list-item-${skill}`, `list-item-${skill}-inverted`);
+        }
+
+        //Update to new class
+        e.target.className = newClass;
+    }
 
     return (
         <div>
@@ -12,17 +30,17 @@ const Skills = () => {
                 <div className="row">
                     <div className="col">
                     <ul className="list-group text-center">
-                        <li className="list-group-item list-item-html5 font-weight-bold mb-2 w-75 mx-auto">HTML5</li>
-                        <li className="list-group-item list-item-css3 font-weight-bold mb-2 w-75 mx-auto">CSS3</li>
-                        <li className="list-group-item list-item-javascript font-weight-bold mb-2 w-75 mx-auto">JavaScript</li>
-                        <li className="list-group-item list-item-sass font-weight-bold mb-2 w-75 mx-auto">Sass</li>
-                        <li className="list-group-item list-item-github font-weight-bold mb-2 w-75 mx-auto">Github</li>
-                        <li className="list-group-item list-item-git font-weight-bold mb-2 w-75 mx-auto">Git</li>
-                        <li className="list-group-item list-item-bootstrap font-weight-bold mb-2 w-75 mx-auto">Bootstrap</li>
-                        <li className="list-group-item list-item-react font-weight-bold mb-2 w-75 mx-auto">React</li>
-                        <li className="list-group-item list-item-node font-weight-bold mb-2 w-75 mx-auto">Node</li>
-                        <li className="list-group-item list-item-express font-weight-bold mb-2 w-75 mx-auto">Express</li>
-                        <li className="list-group-item list-item-sql font-weight-bold mb-2 w-75 mx-auto">SQL</li>
+                        <li className="list-group-item list-item-html5 font-weight-bold mb-2 w-75 mx-auto" onClick={handleClick}>HTML5</li>
+                        <li className="list-group-item list-item-css3 font-weight-bold mb-2 w-75 mx-auto" onClick={handleClick}>CSS3</li>
+                        <li className="list-group-item list-item-javascript font-weight-bold mb-2 w-75 mx-auto" onClick={handleClick}>JavaScript</li>
+                        <li className="list-group-item list-item-sass font-weight-bold mb-2 w-75 mx-auto" onClick={handleClick}>Sass</li>
+                        <li className="list-group-item list-item-github font-weight-bold mb-2 w-75 mx-auto" onClick={handleClick}>Github</li>
+                        <li className="list-group-item list-item-git font-weight-bold mb-2 w-75 mx-auto" onClick={handleClick}>Git</li>
+                        <li className="list-group-item list-item-bootstrap font-weight-bold mb-2 w-75 mx-auto" onClick={handleClick}>Bootstrap</li>
+                        <li className="list-group-item list-item-react font-weight-bold mb-2 w-75 mx-auto" onClick={handleClick}>React</li>
+                        <li className="list-group-item list-item-node font-weight-bold mb-2 w-75 mx-auto" onClick={handleClick}>Node</li>
+                        <li className="list-group-item list-item-express font-weight-bold mb-2 w-75 mx-auto" onClick={handleClick}>Express</li>
+                        <li className="list-group-item list-item-sql font-weight-bold mb-2 w-75 mx-auto" onClick={handleClick}>SQL</li>
                     </ul>
                     </div>
                 </div>
